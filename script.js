a3492ec3d81cd1f0b6e6081bcdb5e6df3a18b9d1
YanChen <yc.yanchen@hotmail.com>
2022-12-01 23:01:15 -0500

    Changed on hover effect for the project overlays to a toggle.

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 document.querySelector(".hamburger-menu").addEventListener("click", () => {
-	const selection = document.querySelector(".nav-bar-container").classList[1];
-	if (selection == "nav-closed") {
-		document.querySelector(".nav-bar-container").className = "nav-bar-container nav-opened";
+	const selection = document.querySelector(".nav-bar-container").classList[3];
+	if (selection == "element-closed") {
+		document.querySelector(".nav-bar-container").className = "nav-bar-container element-opened";
 		document.querySelector(".hamburger-menu").className = "hamburger-menu hamburger-opened";
 	} else {
-		document.querySelector(".nav-bar-container").className = "nav-bar-container nav-closed";
+		document.querySelector(".nav-bar-container").className = "nav-bar-container element-closed";
 		document.querySelector(".hamburger-menu").className = "hamburger-menu hamburger-closed";
 	}
+});
+
+document.querySelector("#project-item-1").addEventListener("click", () => {
+	document.querySelector("#project-overlay-1").classList.toggle("element-closed");
+});
+document.querySelector("#project-item-2").addEventListener("click", () => {
+	document.querySelector("#project-overlay-2").classList.toggle("element-closed");
+});
+document.querySelector("#project-item-3").addEventListener("click", () => {
+	document.querySelector("#project-overlay-3").classList.toggle("element-closed");
 });
