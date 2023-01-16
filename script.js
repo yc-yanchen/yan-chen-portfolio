1b96ead098a4a67ca3848005ed76348128942450
YanChen <yc.yanchen@hotmail.com>
2023-01-16 16:31:22 -0500

    Updated JS

--- script.js
+++ script.js
@@ -12,3 +12,6 @@ document.querySelector("#project-item-2").addEventListener("click", () => {
 document.querySelector("#project-item-3").addEventListener("click", () => {
 	document.querySelector("#project-overlay-3").classList.toggle("element-closed");
 });
+document.querySelector("#project-item-4").addEventListener("click", () => {
+	document.querySelector("#project-overlay-4").classList.toggle("element-closed");
+});
